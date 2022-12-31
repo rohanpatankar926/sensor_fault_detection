694e0d251acd9dd3120ad0a213b2f0f361066ab6
rohanpatankar <rohanpatankar926@gmail.com>
2022-12-31 15:09:41 +0530

    model evaluation and model pusher pipeline

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from sensor.components.data_validation import DataValidation
 from sensor.components.data_transformation import DataTransformation
 from sensor.components.model_trainer import ModelTrainer
 from sensor.components.model_evaluation import ModelEvaluation
-
+from sensor.components.model_pusher import ModelPusher
 if __name__=="__main__":
      try:
           #Data Ingestion
@@ -41,6 +41,13 @@ if __name__=="__main__":
           data_transformation_artifact=data_transformation_artifact,
           model_trainer_artifact=model_trainer_artifact)
           model_eval_artifact = model_eval.initiate_model_evaluation()
-               
+          
+          #model Pusher
+          model_pusher = ModelPusher(model_pusher_config=model_pusher_config, 
+                data_transformation_artifact=data_transformation_artifact,
+                model_trainer_artifact=model_trainer_artifact)
+          model_pusher_artifact = model_pusher.initiate_model_pusher()
+     
      except Exception as e:
-          print(e)
+        raise SensorException(e, sys)
+
